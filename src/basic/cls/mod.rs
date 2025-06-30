a081af97124c2162f37389399ccc7f884a9e1180
单子亨 <shanziheng1721@gmail.com>
2025-06-30 11:59:38 +0100

    Added a skeleton for the type system

---
+++ src/basic/cls/mod.rs
@@ -0,0 +1,5 @@
+pub mod custom_class;
+pub mod prefix;
+pub mod primitive_class;
+
+pub mod class;
--- src/basic/mod.rs
+++ src/basic/mod.rs
@@ -1,6 +1,6 @@
+pub mod cls;
 pub mod cnst;
 pub mod expr;
-pub mod types;
 pub mod var;
 
 pub mod stmt;
